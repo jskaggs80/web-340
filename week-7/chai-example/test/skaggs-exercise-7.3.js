e42f4ae96ee93cb8273010ffe02e946a7830905a
jskaggs80 <jskaggs80@yahoo.com>
2018-09-15 10:23:29 -0500

    Adding Mocha & Chai example

--- week-7/chai-example/test/skaggs-exercise-7.3.js
+++ week-7/chai-example/test/skaggs-exercise-7.3.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title: Exercise 7.3
+; Author: Professor Krasso
+; Date: 15 Sept 2018
+; Modified By: Jake Skaggs
+; Description: chai-example-test
+;===========================================
+*/ 
+
 var fruits = require("../skaggs-fruits");
 
 var chai = require("chai");
