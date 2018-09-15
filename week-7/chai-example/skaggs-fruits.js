18f0263eb5e469f5ba4904bcd94cca39333e7c08
jskaggs80 <jskaggs80@yahoo.com>
2018-09-15 10:20:28 -0500

    Adding Mocha & Chai example

---
+++ week-7/chai-example/skaggs-fruits.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title: Exercise 7.3
+; Author: Professor Krasso
+; Date: 15 Sept 2018
+; Modified By: Jake Skaggs
+; Description: chai-example
+;===========================================
+*/ 
+
+function fruits(str) {
+    return str.split(',');
+}
+
+module.exports = fruits;
