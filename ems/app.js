074767c27dc3ab1e41e71b04a7c4160b1b1da9db
jskaggs80 <jskaggs80@yahoo.com>
2018-09-29 22:01:45 -0500

    Week 9 EMS finishing touches

--- ems/app.js
+++ ems/app.js
@@ -68,6 +68,8 @@ app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 8080);
 
 
+
+
 //http calls
 app.get("/", function (request, response) {
     response.render("index", {
@@ -142,6 +144,6 @@ app.get("/view/:queryName", function (request, response) {
 });
 
 //create server
-http.createServer(app).listen(8080, function() {
-    console.log("Application started on port 8080!");
+http.createServer(app).listen(app.get('port'), function() {
+    console.log("Application started on port " + app.get('port'));
 });
